1a182d3f19765c8229d19d3b115dc4fc9872e577
fbartusch <felix.bartusch@uni-tuebingen.de>
2024-09-22 13:38:46 +0200

    Fix various things
    * Correct example namespace prefix to ex

--- src/main/java/org/provtools/provone/vanilla/ProvOneFactory.java
+++ src/main/java/org/provtools/provone/vanilla/ProvOneFactory.java
@@ -192,22 +192,22 @@ public class ProvOneFactory extends org.openprovenance.prov.vanilla.ProvFactory 
         Collection<Attribute> attrs=new LinkedList<>();
         attrs.add(newAttribute(Attribute.AttributeKind.PROV_LABEL, newInternationalizedString(label), getName().XSD_STRING));
 
-        QualifiedName id = newQualifiedName("http://example.com/", scriptID, "exa");
+        QualifiedName id = newQualifiedName("http://example.com/", scriptID, "ex");
 
         if(location != null) {
             attrs.add(newAttribute(Attribute.AttributeKind.PROV_LOCATION, newInternationalizedString(location), getName().PROV_LOCATION));
         }
 
         if(repository != null) {
-            attrs.add(newAttribute("http://example.com/", "repository", "exa", repository,
+            attrs.add(newAttribute("http://example.com/", "repository", "ex", repository,
                                    newQualifiedName("https://schema.org/", "Text", "schema")));
         }
         if(commitID != null) {
-            attrs.add(newAttribute("http://example.com/", "commitID", "exa", commitID,
+            attrs.add(newAttribute("http://example.com/", "commitID", "ex", commitID,
                                    newQualifiedName("https://schema.org/", "Text", "schema")));        
         }
         if(revision != null) {
-            attrs.add(newAttribute("http://example.com/", "revision", "exa", revision,
+            attrs.add(newAttribute("http://example.com/", "revision", "ex", revision,
                                    newQualifiedName("https://schema.org/", "URL", "schema")));                         
         }
 
